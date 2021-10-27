418bbd6cb61d2afb29bf3746a637c54b1a3baf32
Eike Thies <thies.eike@gmail.com>
2021-10-27 15:05:15 +0200

    compiling now fixed by unresolving fs & path

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const webpack = require("@nativescript/webpack");
 module.exports = (env) => {
 	webpack.init(env);
+	// config.node = { fs: "empty" };
+	webpack.mergeWebpack({ resolve: {fallback:{fs:false,path:false}} })
+	// resolve.fallback.fs = false;
 	return webpack.resolveConfig();
 };
 
