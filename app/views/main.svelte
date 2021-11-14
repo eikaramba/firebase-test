7ad0df61d3b4bbdcb18208b66e7cc51049f2b202
Eike Thies <thies.eike@gmail.com>
2021-11-14 22:45:27 +0100

    test auth permission bug

--- app/views/main.svelte
+++ app/views/main.svelte
@@ -14,24 +14,28 @@
     import '@nativescript/firebase-messaging';
 
     onMount(async ()=>{
-        firebase().initializeApp();
-        const crashlytics = firebase().crashlytics();
-        const messaging = firebase().messaging();
-
-        const authStatus = await messaging.requestPermission({
-			ios: {
-				alert: true,
-			},
-		});
-        const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-        if (enabled) {
-            messaging.onTokenRefresh(async token => {
-                console.log('[Firebase] onTokenRefresh:', { token });
-            });
-            messaging.onMessage(async (message) => {
-                console.log('[Firebase] onMessage:', { message });
-            });
+        try {
+            firebase().initializeApp();
+            const crashlytics = firebase().crashlytics();
+            const messaging = firebase().messaging();
+    
+            const authStatus = await messaging.requestPermission({
+    			ios: {
+    				alert: true,
+    			},
+    		});
+            const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    
+            if (enabled) {
+                messaging.onToken(async token => {
+                    console.log('[Firebase] onTokenRefresh:', { token });
+                });
+                messaging.onMessage(async (message) => {
+                    console.log('[Firebase] onMessage:', { message });
+                });
+            }
+        } catch (err) {
+            console.log('[Firebase] Error:', err);
         }
     })
 
